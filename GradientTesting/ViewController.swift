be3eaa19264878058ed6257832c9a45af80e86e6
Michael Miles <michael.miles@seasoned.co>
2022-03-24 12:45:40 -0400

    Initial Commit

---
+++ GradientTesting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GradientTesting
+//
+//  Created by Michael Miles on 3/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
